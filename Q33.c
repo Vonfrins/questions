5438f62764af674c4ccbd108a20bbc0f6d4e75c8
Vonfrins <128450992+Vonfrins@users.noreply.github.com>
2023-03-23 12:48:35 +0800

    Add files via upload

---
+++ Q33.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+// 求二进制中不同位的个数，两个int（32位）整数m,n的二进制表达式中，有多少个位 (bit)不同
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int m;
+//	int n;
+//
+//
+//
+//	return 0;
+//}
