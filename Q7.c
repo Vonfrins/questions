70620b0779134c13d8abf496d9b16a7a288d7593
yifan <yifanwang@gmail.com>
2023-04-02 23:57:13 +0800

    daily

--- Q7.c
+++ Q7.c
@@ -1,26 +1,26 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-
-#include<stdio.h>
-
-//  數呾n腔論傚
-//  Calculate the factorial of n
-
-
-
-int main()
-{
-	int n = 0;
-	int i = 0;
-
-	scanf("%d", &n);
-	int m = n;
-
-	for (i=1;i<n;i++)
-	{
-		m *= i;
-	}
-	
-	printf("%d", m);
-
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include<stdio.h>
+//
+////  數呾n腔論傚
+////  Calculate the factorial of n
+//
+//
+//
+//int main()
+//{
+//	int n = 0;
+//	int i = 0;
+//
+//	scanf("%d", &n);
+//	int m = n;
+//
+//	for (i=1;i<n;i++)
+//	{
+//		m *= i;
+//	}
+//	
+//	printf("%d", m);
+//
+//	return 0;
+//}
